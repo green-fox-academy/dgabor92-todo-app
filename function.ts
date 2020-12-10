2bee278392694c8d80cdb2464b2578dfc442f161
Deutsch Gábor <deutschgabor92@gmail.com>
2020-12-10 12:00:04 +0100

    Command print és task list

---
+++ function.ts
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs');
+
+export default class Todofunction {
+  // print the argument lists
+  printCommands(): void {
+    console.log(
+      'Command Line Todo application' +
+        '\n' +
+        '=============================' +
+        '\n' +
+        '\n' +
+        'Command line arguments:' +
+        '\n' +
+        '    -l   Lists all the tasks' +
+        '\n' +
+        '    -a   Adds a new task' +
+        '\n' +
+        '    -r   Removes an task' +
+        '\n' +
+        '    -c   Completes an task'
+    );
+    console.log('________________________');
+  }
+  //read from the predefined file the tasks and log it
+  listTasks(): void {
+    let context: string = fs.readFileSync('task.txt', 'utf-8');
+    if (context == '') {
+      console.log('No todos for today! ;)');
+    } else {
+      let contextLines: string[] = [];
+      context.split('\n').forEach(function (lines) {
+        contextLines.push(lines);
+      });
+      for (let i: number = 0; i < contextLines.length; i++) {
+        console.log(i + 1 + '.-' + contextLines[i]);
+      }
+    }
+    console.log('________________________');
+  }
+}
+
+let a = new Todofunction();
+a.printCommands();
+a.listTasks();
--- todo.ts
+++ todo.ts
@@ -1,10 +1,37 @@
 'use strict';
 
 // process.argv -> array az eredmenye
+// console.log(process.argv);
+
+// if (process.argv[2] === '-l') {
+//   console.log('Here comes the description');
+// } else {
+//   console.log('I dont know what to do');
+// }
+
 console.log(process.argv);
 
-if (process.argv[2] === '-l') {
-  console.log('Here comes the description');
+if (process.argv[2] === '-a') {
+  console.log('Walk the dog');
+  console.log('Buy milk');
+  console.log('Do homework');
 } else {
-  console.log('I dont know what to do');
+  console.log('There is no task in the file');
+}
+
+export default class Todo {
+  protected _lists = process.argv[2] === '-l';
+  protected _add = process.argv[2] === '-a';
+  protected _remove = process.argv[2] === '-r';
+  protected _complete = process.argv[2] === '-c';
+
+  addTask() {
+    if (this._add) {
+      console.log('Walk the dog');
+      console.log('Buy milk');
+      console.log('Do homework');
+    } else {
+      console.log('There is no task in the file');
+    }
+  }
 }
